9c26d3383e5e7787cd4f75f7ba68e376b9b00a36
tmek1244 <tmek1244@wp.pl>
2023-09-11 00:39:33 +0200

    Add zsh config

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# easier to read disk
+alias df='df -h'     # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+alias m="git checkout master"
+
---
+++ zsh/.config/zsh/exports.zsh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+HISTSIZE=1000000
+SAVEHIST=1000000
+export EDITOR="lvim"
+export PATH="$HOME/.local/bin":$PATH
+
