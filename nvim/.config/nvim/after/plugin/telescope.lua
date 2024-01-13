307c6c2fa78dc645ec15a8764685a60fa8713118
tmek1244 <tmek1244@wp.pl>
2024-01-13 20:00:47 +0100

    Add git status search mapping

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -5,4 +5,5 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 
